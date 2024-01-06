b61e470b3fc288415c8c3967677c3bf031c4c03a
Sherif Hamdy <shamdy@xapps.co>
2024-01-06 16:28:42 +0200

    Initial Commit

---
+++ Swiftui-Continue-Learning/Swiftui_Continue_LearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swiftui_Continue_LearningApp.swift
+//  Swiftui-Continue-Learning
+//
+//  Created by Sherif Hamdy on 06/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct Swiftui_Continue_LearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
