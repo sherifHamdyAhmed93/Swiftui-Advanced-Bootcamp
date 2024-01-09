4b0aea3b9e9e08682b39b565314fc0d1f1e540f6
Sherif Hamdy <shamdy@xapps.co>
2024-01-09 23:12:40 +0200

    Rotation Gesture

---
+++ Swiftui-Continue-Learning/RotationGestureBootcamp.swift
@@ -0,0 +1,38 @@
+//
+//  RotationGestureBootcamp.swift
+//  Swiftui-Continue-Learning
+//
+//  Created by Sherif Hamdy on 07/01/2024.
+//
+
+import SwiftUI
+
+struct RotationGestureBootcamp: View {
+    @State var angle:Angle = Angle(degrees: 0)
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+            .font(.title)
+            .padding(40)
+            .background(Color.red.cornerRadius(10))
+            .rotationEffect(angle)
+            .gesture(
+                RotationGesture()
+                    .onChanged({ value in
+                        angle = value
+                    })
+                    .onEnded({ value in
+                        withAnimation(.spring()) {
+                            angle = Angle(degrees: 0)
+                        }
+                    })
+            )
+        
+    }
+}
+
+struct RotationGestureBootcamp_Previews: PreviewProvider {
+    static var previews: some View {
+        RotationGestureBootcamp()
+    }
+}
